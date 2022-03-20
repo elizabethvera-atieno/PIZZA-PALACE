b88f4b60d74bc1482bd99125b929de752a1a81ca
elizabethvera-atieno <atienolizvera@gmail.com>
2022-03-20 11:45:09 +0300

    add a Pizza constructor

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Constructor Pizza
+function Pizza(flavor,size,crust,topping, total){
+    this.flavor = flavor;
+    this.size = size;
+    this.crust = crust;
+    this.topping = topping;
+    this.total = total;
+}
+$(document).ready(function(){
+
+})
